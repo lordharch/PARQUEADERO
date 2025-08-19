e4326c85b7513cac8f5f95609e67eb275f5fc0c5
harrison <baldionh@gmail.com>
2025-08-19 18:01:13 -0500

    subiendo trabajos AA2 y AA5

--- ANA DEL PILAR-GA7-220501096-AA3-EV01/parqueadero/src/main/java/com/parqueo/usuario/ParqueaderoApplication.java
+++ ANA DEL PILAR-GA7-220501096-AA3-EV01/parqueadero/src/main/java/com/parqueo/usuario/ParqueaderoApplication.java
@@ -2,6 +2,7 @@ package com.parqueo.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -24,4 +25,14 @@ public class ParqueaderoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParqueaderoApplication.class, args);
     }
+=======
+
+@SpringBootApplication
+public class ParqueaderoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoApplication.class, args);
+	}
+
+>>>>>>> 1851662 (subiendo trabajos AA2 y AA5)
 }
