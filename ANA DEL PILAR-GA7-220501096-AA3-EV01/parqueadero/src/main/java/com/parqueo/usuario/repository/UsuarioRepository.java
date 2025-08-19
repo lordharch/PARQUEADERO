70a8780837b9df33547c98c5d158a46d45eb9436
harrison <baldionh@gmail.com>
2025-08-19 18:44:33 -0500

    Subiendo mis cambios

--- ANA DEL PILAR-GA7-220501096-AA3-EV01/parqueadero/src/main/java/com/parqueo/usuario/repository/UsuarioRepository.java
+++
@@ -1,5 +0,0 @@
-package com.parqueo.usuario.repository;
-import com.parqueo.usuario.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
-
